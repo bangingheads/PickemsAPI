2fbb35d5d1d76a24b2d4b223cb11c6c0c349f23c
BangingHeads <53184036+bangingheads@users.noreply.github.com>
2021-10-20 13:51:56 -0500

    Multiple pickblocks/stages, add team info endpoint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from typing import Optional
 from time import time
 import aiohttp
 import re
@@ -6,7 +7,6 @@ import re
 username = "bangingheads"
 password = "MySecretPassword"
 leaderboard_id = "577426844126909614"
-pick_block = "8843040735491922676"
 
 class Auth:
     def __init__(self, username: str, password: str) -> None:
@@ -61,19 +61,49 @@ app = FastAPI(
 )
 auth: Auth = None
 players_by_name = {}
+pick_blocks = {}
+teams = {}
 
 @app.on_event("startup")
 async def startup_event():
-    global auth
+    global auth, pick_blocks, teams
     auth = Auth(username, password)
     await auth.get_auth()
+    headers = {"x-api-key": "uvuy3Wep82aZ3D21pAH9Z4RygQZjfbzC2QtedVFs"}
+    tournament_id = ""
+    async with aiohttp.ClientSession() as session:
+        # Set pick blocks
+        async with session.get('https://pickem.lolesports.com/api/v1/leagues?hl=en-US') as resp:
+            data = await resp.json()
+            pick_blocks = {pick_block['slug']['slug']: pick_block['id'] for pick_block in data['leagues'][0]['tournament']['pickBlocks']}
+            tournament_id = data['leagues'][0]['tournament']['eldsTournamentId']
+        # Set Teams
+        async with session.get(f'https://esports-api.lolesports.com/persisted/pickem/getMatchesForTournament?hl=en-US&sport=lol&tournamentId={tournament_id}', headers=headers) as resp:
+            data = await resp.json()
+            events = data['data']['matches']['events']
+            teams.update({team['id']: team for event in events for team in event['match']['teams']})
+        
 
-@app.get("/player-by-id/{id}", summary="Get player by player ID")
-async def player_by_id(id):
+@app.get("/player-by-id/{id}", summary="Get player by ID of latest stage")
+@app.get("/player-by-id/{id}/{stage}", summary="Get player by player ID by stage")
+async def player_by_id(id: str, stage: Optional[str] = None):
+    if stage == None:
+        stage = list(pick_blocks.keys())[-1]
     headers = await auth.get_headers()
     async with aiohttp.ClientSession() as session:
-        async with session.get(f'https://pickem.lolesports.com/api/v1/section-picks/players/{id}/pickBlock/{pick_block}', headers=headers) as resp:
+        async with session.get(f'https://pickem.lolesports.com/api/v1/section-picks/players/{id}/pickBlock/{pick_blocks[stage]}', headers=headers) as resp:
             return await resp.json()
+
+@app.get("/player-by-name/{name}", summary="Get player by name in leaderboard of latest stage")
+@app.get("/player-by-name/{name}/{stage}", summary="Get player by name that is part of the leaderboard by stage")
+async def by_name(name: str, stage: Optional[str] = None):
+    if stage == None:
+        stage = list(pick_blocks.keys())[-1]
+    await get_leaderboard()
+    if name.lower() in players_by_name:
+         return await player_by_id(players_by_name[name.lower()], stage)
+    else:
+        raise HTTPException(status_code=404, detail="Player not found")
 
 @app.get("/leaderboard", summary="Get leaderboard set in config")
 async def get_leaderboard():
@@ -82,7 +112,7 @@ async def get_leaderboard():
     players_by_name = {player['player']['name'].lower(): player['player']['id'] for player in data['standings']}
     return data
 
-@app.get('/leaderboard-by-id/{id}', summary="Get leaderboard by ID")
+@app.get("/leaderboard-by-id/{id}", summary="Get leaderboard by ID")
 async def leaderboard_by_id(id):
     headers = await auth.get_headers()
     async with aiohttp.ClientSession() as session:
@@ -90,10 +120,10 @@ async def leaderboard_by_id(id):
             data = await resp.json()
     return data
 
-@app.get('/player-by-name/{name}', summary="Get player by name that is part of the leaderboard")
-async def by_name(name):
-    await get_leaderboard()
-    if name.lower() in players_by_name:
-         return await player_by_id(players_by_name[name.lower()])
-    else:
-        raise HTTPException(status_code=404, detail="Player not found")
+@app.get("/stages", summary="Get active stages by name")
+def get_stages():
+    return list(pick_blocks.keys())
+
+@app.get("/teams", summary="Get all active teams info by ID")
+def get_teams():
+    return teams
